037ec22f99880dcd65d649915af47e9884384943
tashin <ahamed1807083@stud.kuet.ac.bd>
2022-07-27 19:53:22 +0600

    Fully functional app

---
+++ app/src/main/java/com/example/cardiacmeasurementmanager/RecyclerAdapter.java
@@ -0,0 +1,127 @@
+package com.example.cardiacmeasurementmanager;
+
+import java.util.ArrayList;
+import android.annotation.SuppressLint;
+import android.app.ApplicationErrorReport;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerViewHolder> {
+    private static CustomClickListener customClickListener;
+    private final ArrayList<ModelClass> arrayList;
+    private ModelClass modelClass;
+    private final Context context;
+    private final int greenColor;
+    private final int redColor;
+    public RecyclerAdapter(Context context,  ArrayList<ModelClass> list,int greenC,int redC) {
+        this.arrayList=list;
+        this.context = context;
+        this.greenColor =greenC;
+        this.redColor = redC;
+    }
+    public void setCustomClickListener(CustomClickListener customClickListener)
+    {
+        this.customClickListener = customClickListener;
+    }
+    @NonNull
+    @Override
+    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_view, parent, false);
+        return new RecyclerViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerAdapter.RecyclerViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        if (!arrayList.isEmpty()) modelClass = arrayList.get(position);
+        holder.dateTextView.setText(""+modelClass.getDate());
+        holder.systolicTextView.setText(""+modelClass.getSystolic());
+        holder.diastolicTextView.setText(""+modelClass.getDiastolic());
+        holder.heartTextView.setText(""+modelClass.getHeartRate());
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                if (customClickListener != null) {
+                    if (position != RecyclerView.NO_POSITION) {
+                        customClickListener.onDeleteClick(position);
+                    }
+                }
+
+
+            }
+
+        });
+        if (modelClass.getDiastolic()<=80 && modelClass.getSystolic()<=120 && modelClass.getHeartRate()>=60 && modelClass.getHeartRate()<=100) {
+            Drawable buttonDrawable = holder.containerCardView.getBackground();
+            buttonDrawable = DrawableCompat.wrap(buttonDrawable);
+            DrawableCompat.setTint(buttonDrawable, greenColor);
+            holder.containerCardView.setBackground(buttonDrawable);
+            holder.containerCardView.setCardBackgroundColor(greenColor);
+            // Toast.makeText(context,"color",Toast.LENGTH_SHORT).show();
+        }
+        else if(!(modelClass.getDiastolic()<90&& modelClass.getSystolic()<139&&modelClass.getHeartRate()>=40&&modelClass.getHeartRate()<110)){
+            Drawable buttonDrawable = holder.containerCardView.getBackground();
+            buttonDrawable = DrawableCompat.wrap(buttonDrawable);
+            DrawableCompat.setTint(buttonDrawable, redColor);
+            holder.containerCardView.setBackground(buttonDrawable);
+            holder.containerCardView.setCardBackgroundColor(redColor);
+            //  Toast.makeText(context,"color",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+
+    public interface CustomClickListener {
+        void cusOnClick(int position, View v);
+        void onDeleteClick(int position);
+    }
+    public class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView systolicTextView;
+        TextView heartTextView;
+        TextView diastolicTextView;
+        TextView dateTextView;
+        CardView containerCardView;
+        ImageView deleteButton;
+        public RecyclerViewHolder(@NonNull View itemView) {
+            super(itemView);
+            systolicTextView = itemView.findViewById(R.id.tvSystolic);
+            diastolicTextView = itemView.findViewById(R.id.tvDiastolic);
+            dateTextView = itemView.findViewById(R.id.tvDate);
+            heartTextView = itemView.findViewById(R.id.tvHeartRate);
+            containerCardView = itemView.findViewById(R.id.llContainerCardView);
+            deleteButton = itemView.findViewById(R.id.deleteIm);
+            itemView.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View view) {
+            customClickListener.cusOnClick(getAdapterPosition(), view);  //position and view setting to provide to mainactivity
+        }
+
+    }
+
+
+}
+
+
